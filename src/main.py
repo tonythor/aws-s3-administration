148ca411ebedf6adedfede6cbbd1931cf384efba
Tony Fraser <tony.fraser@nbcuni.com>
2022-12-01 23:22:31 -0500

    removing main

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import logging
-from tonythor import conf
-from tonythor.s3_api_rules_service import S3ApiRulesService
-
-
-logging.info(f"Using account number: {conf.account_number}")
-service = S3ApiRulesService(bucket = conf.bucket)
-service.upload(conf.inbound_rule)
-
